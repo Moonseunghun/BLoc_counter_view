d68eae4688032809fc8dd7767f9c181ba653c4b1
moonseunghun <mangokkaksae2@gmail.com>
2023-12-18 12:45:59 +0900

    Chore print 변경

--- lib/src/bloc/sample_bloc.dart
+++ lib/src/bloc/sample_bloc.dart
@@ -4,7 +4,7 @@ class SampleBloc extends Bloc<SampleEvent, int> {
   SampleBloc() : super(0) {
     print('init Samplebloc');
     on<SampleEvent>((event, emit) {
-      print('Sample Event Called');
+      print('Event bloc');
     });
     on<AddSampleEvent>((event, emit) {
       emit(state + 1);
